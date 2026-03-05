465119be9d3505549499980dd8bb3d840317631d
Kevin <kevin.rodriguez.quintero@correounivalle.edu.co>
2026-03-05 09:52:45 -0500

    Formulario

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pulp import *
 
 app=Flask(__name__)
@@ -9,22 +9,29 @@ def home():
 
 @app.route("/optimizacion", methods=["GET", "POST"])
 def modelo():
-    #modelacion con pulp
-    ventanas=LpProblem("modelo_de_optimizacion", LpMaximize)
-    x1=LpVariable ("Ventanas_madera", 0)
-    x2=LpVariable ("Ventanas_aluminio", 0)
 
-    ventanas+=60*x1 +30*x2 #F.O
-    ventanas+=x1<=6 #R1
-    ventanas+=x2<=4 #R2
-    ventanas+=6*x1+8*x2<=48 #R3
+    if request.method=="POST":
+        madera=float(request.form.get("madera"))
+        aluminio=float(request.form.get("aluminio"))
 
-    ventanas.solve()
-    
-    res1=x1.varValue
-    res2=x2.varValue
+        #modelacion con pulp
+        ventanas=LpProblem("modelo_de_optimizacion", LpMaximize)
+        x1=LpVariable ("Ventanas_madera", 0)
+        x2=LpVariable ("Ventanas_aluminio", 0)
 
-    return render_template("Optimizacion.html", res1=res1, res2=res2)
+        ventanas+= madera*x1+aluminio*x2 #F.O
+        ventanas+=x1<=6 #R1
+        ventanas+=x2<=4 #R2
+        ventanas+=6*x1+8*x2<=48 #R3
+
+        ventanas.solve()
+        
+        res1=x1.varValue
+        res2=x2.varValue
+
+        return render_template("Optimizacion.html", res1=res1, res2=res2)
+    return render_template("Optimizacion.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
